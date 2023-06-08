7576e50efff09fb2e54c3824867714b2d9cbdb0f
OjashviSingh <ojashvisingh365@gmail.com>
2023-06-08 18:38:37 +0530

    Added: Demo Spring App

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -2,29 +2,40 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Person;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
 
-    @GetMapping(value = {"", "/", "/hello"})
-    public String getApi1(){
-        return "Hello SpringBoot";
-    }
     @RequestMapping(value = {"", "/", "/hello"})
-    public String getApi2(){
+    public String getApi1() {
         return "Hello Spring Boot";
     }
-    @PostMapping("/add-person")
-    public String addPerson(@RequestBody Person person){
-        return "Hi Spring Boot";
+
+    @GetMapping(value = {"", "/", "/hello"})
+    public String getApi2() {
+        return "Hello SpringBoot";
     }
+
+    @PostMapping("/add-person")
+    public String addPerson(@RequestBody Person person) {
+        return person.getfName() + " " + person.getlName();
+    }
+
+    @PutMapping("/users/{id}")
+    public String updateUser(@PathVariable Long id, @RequestBody Person person2) {
+
+        return "User updated successfully";
+    }
+
     @GetMapping(value = {"/api2/{name}"})
-    public String getApi3(@PathVariable(name = "name") String name){
-        if(name.equals("ojashvi")){
-            return "Hi Ojashvi";
-        }  else {
-            return "nothing";
-        }
+    public String getApi3(@RequestParam String fName, String lName) {
+        return "Hello " + fName + " " + lName;
+    }
+
+    @GetMapping(value = {"/api2/{name}"})
+    public String getApi4(@PathVariable String fName, String lName) {
+        return "Hello " + fName + " " + lName;
     }
 }
--- src/main/java/com/example/demo/entity/Person.java
+++ src/main/java/com/example/demo/entity/Person.java
@@ -1,4 +1,22 @@
 package com.example.demo.entity;
 
 public class Person {
+    private String fName;
+    private String lName;
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
 }
